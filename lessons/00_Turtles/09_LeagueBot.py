c2f72895cc5e718449d5c192184057362359f0bf
Nathanielwang14 <nathanielwang14@gmail.com>
2024-10-25 23:58:53 +0000

    8a-10a

--- lessons/00_Turtles/09_LeagueBot.py
+++ lessons/00_Turtles/09_LeagueBot.py
@@ -11,11 +11,41 @@ Write your own turtle program! Here is what your program should do
 
 import turtle as turtle
 
-screen = turtle.Screen()
-screen.setup(width=600, height=600)
-screen.bgcolor('white')
+# screen = turtle.Screen()
+# screen.setup(width=600, height=600)
+# screen.bgcolor('white')
 
 t = turtle.Turtle()
 
 ... # Your Code Here
-def turtle
+import turtle
+
+def set_background_image(window, image_name):
+
+    from pathlib import Path
+    from PIL import Image
+
+
+    image_dir = Path(__file__).parent / "images"
+    image_path = str(image_dir / image_name)
+
+    image = Image.open(image_path)
+    
+    window.setup(image.width, image.height, startx=0, starty=0)
+    window.bgpic(image_path)
+   
+
+tina = turtle.Turtle()                 
+
+screen = turtle.Screen()                
+set_background_image(screen, "leaguebot_bolt.gif") 
+
+tina.turtlesize(stretch_wid=10, stretch_len=10, outline=4)
+tina.pencolor('blue')
+
+tina.pendown
+for i in range(6):
+    tina.forward(40)
+    tina.left(60)
+
+turtle.done()      
--- lessons/00_Turtles/10a_Tash_Me.py
+++ lessons/00_Turtles/10a_Tash_Me.py
@@ -11,3 +11,45 @@ Hint: See 08a_More Turtle Programs, section 'Change the Background Image' and
 
 ... # Your code here
 
+import turtle
+
+def set_background_image(window, image_name):
+
+    from pathlib import Path
+    from PIL import Image
+
+
+    image_dir = Path(__file__).parent / "images"
+    image_path = str(image_dir / image_name)
+
+    image = Image.open(image_path)
+    
+    window.setup(image.width, image.height, startx=0, starty=0)
+    window.bgpic(image_path)
+
+# Set up the screen
+import turtle                           
+turtle.setup(width=600, height=600)     
+
+tina = turtle.Turtle()                 
+
+screen = turtle.Screen()                
+set_background_image(screen, "emoji.png") 
+
+def set_turtle_image(turtle, image_name):
+    """Set the turtle's shape to a custom image."""
+
+    from pathlib import Path
+    image_dir = Path(__file__).parent / "images"
+    image_path = str(image_dir / image_name)
+
+    screen = turtle.getscreen()
+    screen.addshape(image_path)
+    turtle.shape(image_path)
+
+set_turtle_image(tina, "moustache2.gif")
+
+          
+
+
+turtle.exitonclick()
