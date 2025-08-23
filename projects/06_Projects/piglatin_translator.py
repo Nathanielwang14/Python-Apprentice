a8b5738ff312783909b975f2ff8662f0e4a6f6b4
Nathanielwang14 <nathanielwang14@gmail.com>
2025-08-23 00:03:20 +0000

    1

--- projects/06_Projects/piglatin_translator.py
+++ projects/06_Projects/piglatin_translator.py
@@ -1,7 +1,10 @@
 # colin gave me this stupid projectr to make sure i knew how to code in python
 # before i started hotel manager 
-
 import string
+from tkinter import simpledialog, Tk
+window = Tk()
+window.withdraw()
+
 
 vowel_set = 'aeiouAEIOU'
 vowels = [ch for ch in string.ascii_letters if ch in vowel_set]
@@ -11,21 +14,6 @@ import string
 vowels = 'aeiouAEIOU'
 consonants = [ch for ch in string.ascii_letters if ch not in vowels]
 
-
-
-def ask_word(prompt):
-    #prompt to ask 
-    while True:
-        try:
-            return int(input(prompt))
-        except ValueError:
-            print("Please enter a actual word!")
-
-
-
-
-i= ask_word("enter word")
-x= input
 vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
 consonants = [
     'b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 
@@ -34,15 +22,26 @@ consonants = [
     'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'X', 'Y', 'Z'
 ]
 
-
-while True:
-        print(i)
-        if x in vowels:
-             
-             
-             
-
-
-
-
-
+def ask_word(prompt):
+    #prompt to ask 
+    while True:
+        try:
+            return str(simpledialog.askstring("Textbox", prompt))
+        except ValueError:
+            print("Please enter a actual word!")
+        
+def ask_phrase(prompt):
+      while True:
+        try:
+            return str(simpledialog.askstring("Textbox", prompt))
+        except ValueError:
+            print("Please enter a actual word!")
+    
+def entered_word(value):
+    for word in value.split():
+        if word[0] in vowels:
+            print(word + "yay", end=" ") 
+        else:
+            print(word[1:] + word[0] + "ay", end=" ") 
+ 
+entered_word(ask_word("Enter Word"))
